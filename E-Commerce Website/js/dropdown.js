836a59ddb2af1e5224bead82c005de9e696b9d77
Kai-Lee MacBain <kmacbain@sfu.ca>
2021-03-23 10:52:09 -0700

    Kai Final Version of Site
    Finished all modules and page content

--- E-Commerce Website/js/dropdown.js
+++ E-Commerce Website/js/dropdown.js
@@ -2,66 +2,66 @@
 
 'use strict';
 
-// object.method();
-// Form arguments take
+// Find every dropdown menu list in the HTML
+const all_menus = document.querySelectorAll('.dropdown_wrapper');
 
-console.log('Hello World, Javascript is online');
+//Running dropdown script + listener on every instance of .dropdown_wrapper that exists
 
+//Help understanding how to step through each drop down menu list by first converting the nodelist to an array and then returning each item and their index from my wife, Bri MacBain and from the follwing tutorials:
+//https://gomakethings.com/converting-a-nodelist-to-an-array-with-vanilla-javascript/
+//https://gomakethings.com/es6-foreach-loops-with-vanilla-javascript/
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+Array.from(all_menus).forEach((element, index) => {
 
-// var menu = document.querySelector ('#office');
-// var is also an option for the variable, but let is better
+	// Assign each element in the array to the variable menu
+	let menu = element;
 
-let menu = document.querySelector ('#office');
+	// Set each item in the menu list to hidden
+	menu.classList.add('hidden');
 
-menu.classList.add('hidden');
+	// Accessability settings when menu is hidden
+	menu.setAttribute('aria-hidden', 'true');
+	menu.setAttribute('aria-labelledby', 'menu-toggle');
 
-menu.setAttribute('aria-hidden', 'true');
-menu.setAttribute('aria-labelledby', 'menu-toggle');
+	// Take the first item in the menu and set it to variable menuFirstItem
+	let menuFirstItem = element.querySelector('a:first-of-type');
 
-let menuFirstItem = document.querySelector('#office a:first-of-type');
-let header = document.querySelector('header');
+	// Toggle hidden menu on and off when clicking menu title (ie 'office')
+	let menuToggle = menu.parentNode;
 
-
-let menuToggle = document.createElement('button');
-
-
-menuToggle.classList.add('button');
-
-menuToggle.setAttribute('id', 'menu-toggle');
-
-menuToggle.innerHTML = "Menu";
-
-menuToggle.setAttribute('aria-label', 'Menu');
-menuToggle.setAttribute('aria-controls', 'menu');
-menuToggle.setAttribute('aria-expanded', 'false');
-
-header.insertBefore(menuToggle, menu);
+	//Accessability settings for when menu is displayed
+	menuToggle.setAttribute('aria-label', 'menu-toggle');
+	menuToggle.setAttribute('aria-controls', 'menu');
+	menuToggle.setAttribute('aria-expanded', 'false');
 
 
-// Adding the interactive part that hides and shows for menuToggle
+	// Adding the interactive part that hides and shows for menuToggle
 
-menuToggle.addEventListener('click', 
-	function(){
-		console.log('menuToggle has been clicked.');
+	menuToggle.addEventListener('click', 
+		function(){
+			console.log('menuToggle has been clicked.');
 
-		if(menu.classList.contains('hidden')){
-			console.log('Menu is hidden, showing the menu');
+			if(menu.classList.contains('hidden')){
+				console.log('Menu is hidden, showing the menu');
 
-			menu.classList.remove('hidden');
+				menu.classList.remove('hidden');
 
-			// Accessibility controls
-			menu.setAttribute('aria-hidden', 'false');
-			menu.setAttribute('aria-expanded', 'true');
-			menuFirstItem.focus();
-		}else{
-			console.log('Menu is shown, hiding the menu');
+				// Accessibility controls
+				menu.setAttribute('aria-hidden', 'false');
+				menu.setAttribute('aria-expanded', 'true');
+				menuFirstItem.focus();
+			}else{
+				console.log('Menu is shown, hiding the menu');
 
-			menu.classList.add('hidden');
+				menu.classList.add('hidden');
 
-			menu.setAttribute('aria-hidden', 'true');
-			menu.setAttribute('aria-expanded', 'false');
+				menu.setAttribute('aria-hidden', 'true');
+				menu.setAttribute('aria-expanded', 'false');
+			}
 		}
-	}
 
 
-	);
+		);
+
+});
+
